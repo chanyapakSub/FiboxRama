647ac980f0846671112d9dff42021eb5aab29e5a
NING <111240690+chanyapakSub@users.noreply.github.com>
2026-03-03 23:26:02 +0700

    ีup

---
+++ src/lib/prisma.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client"
+import { PrismaNeon } from "@prisma/adapter-neon"
+import { Pool } from "@neondatabase/serverless"
+
+const connectionString = process.env.DATABASE_URL!
+
+const pool = new Pool({
+  connectionString,
+})
+
+const adapter = new PrismaNeon(pool)
+
+export const prisma = new PrismaClient({
+  adapter,
+})
